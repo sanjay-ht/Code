3989e2517f0586bce813edb92b06b5a6e589956a
Supriya Koli <devs@nexsphereinn.com>
2025-04-28 18:31:52 +0530

    otp part removed

--- backend/src/main/java/com/dev/usersmanagementsystem/repository/OtpRepo.java
+++
@@ -1,8 +0,0 @@
-package com.dev.usersmanagementsystem.repository;
-
-import com.dev.usersmanagementsystem.entity.Otp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OtpRepo extends JpaRepository<Otp, String> {
-
-}
